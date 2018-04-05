16524d67a48d6943519c57c5874dd9911f6e454d
student <student@student-11.local>
2018-04-05 11:05:37 +0300

    Check my Game

--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -9,8 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var treeImageView: UIImageView!
+    @IBOutlet weak var correctWordLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        sender.isEnabled = false
+    }
+    
+    var listOfWords = [
+    "Анастасия",
+    "Анна",
+    "Мария",
+    ]
+    
+    let incorrectMovesAllowed = 7
+    
+    var totalWins = 0
+    var totalLosses = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    func newRound() {
+        
     }
 }
